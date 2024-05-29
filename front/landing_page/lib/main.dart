61b9f61385d4c79854ae777f08fd7021f27c81e2
drydry <audry.a.wijaya@gmail.com>
2024-05-29 20:13:00 +0700

    updates

--- front/landing_page/lib/main.dart
+++ front/landing_page/lib/main.dart
@@ -10,16 +10,47 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 213, 224, 233),
-        body: Center(
-          child: Text(
-            'American Sign Language Intepreter',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold
+        body: CloudBackground(
+          child: Center(
+            child: Text(
+              'American Sign Language Interpreter',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-             )))
-      );
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CloudBackground extends StatelessWidget {
+  final Widget child;
+  const CloudBackground({required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          color: Color.fromARGB(255, 213, 224, 233),
+        ),
+        Positioned(
+          top: 50,
+          left: 50,
+          child: Image.asset('assets/cloud1.png', width: 100, height: 100),
+        ),
+        Positioned(
+          top: 150,
+          right: 50,
+          child: Image.asset('assets/cloud2.png', width: 150, height: 150),
+        ),
+        // Add more Positioned widgets for additional clouds
+        child,
+      ],
+    );
   }
 }
